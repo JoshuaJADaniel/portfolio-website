f11ad9744a17ccea55fe58c8c5cb9a7b0ac18148
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-18 20:04:50 -0400

    feat: add navigational menu

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,14 @@ import All from "pages/All";
 import Images from "pages/Images";
 import Skills from "pages/Skills";
 import Extracurriculars from "pages/Extracurriculars";
-import Header from "components/Header/Header";
+
+import Header from "components/Header";
+import Menu from "components/Menu";
 
 const App = () => (
   <Router>
     <Header />
-    <ul>
-      <li>
-        <Link to="/">All</Link>
-      </li>
-      <li>
-        <Link to="/images">Images</Link>
-      </li>
-      <li>
-        <Link to="/skills">Skills</Link>
-      </li>
-      <li>
-        <Link to="/extracurriculars">Extracurriculars</Link>
-      </li>
-    </ul>
-    <hr />
+    <Menu />
     <Switch>
       <Route exact path="/">
         <All />
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,30 @@
+import {
+  Link,
+  matchPath,
+  withRouter,
+  RouteComponentProps,
+} from "react-router-dom";
+import menuOptions from "./menuOptions";
+import styles from "./Menu.module.scss";
+
+const Menu = ({ location: { pathname } }: RouteComponentProps) => (
+  <nav className={styles.container}>
+    {menuOptions.map((item) => {
+      const match = matchPath(item.path, { path: pathname, exact: true });
+      const linkStyle = match ? styles.navLinkActive : styles.navLinkInactive;
+
+      return (
+        <Link to={item.path} className={`${styles.navLink} ${linkStyle}`}>
+          <div className={styles.innerLinkWrapper}>
+            <span className={styles.iconWrapper}>
+              {item.icon(Boolean(match))}
+            </span>
+            {item.text}
+          </div>
+        </Link>
+      );
+    })}
+  </nav>
+);
+
+export default withRouter(Menu);
