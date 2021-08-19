f6ae4ff1e4e7a182760b3d49d69f4ca664789bd9
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-19 13:21:55 -0400

    feat: add sample experiences and projects

---
+++ src/pages/All/experiences.tsx
@@ -0,0 +1,8 @@
+const experiences = Array(1).fill({
+  title: "Lorem Ipsum Dolor",
+  link: "https://example.com/work/experience/",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a nisi a velit malesuada pulvinar quis eu mauris. In sed ultrices ex, suscipit pharetra mi. Nunc in nisl odio. Proin auctor dolor id elementum aliquet.",
+});
+
+export default experiences;
