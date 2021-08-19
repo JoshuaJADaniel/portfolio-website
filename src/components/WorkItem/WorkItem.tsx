23c0621f2a6b27189590d335162914c616449e79
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-19 12:54:28 -0400

    feat: add work item component

---
+++ src/components/WorkItem/WorkItem.tsx
@@ -0,0 +1,24 @@
+import LinkTitle from "components/LinkTitle";
+import styles from "./WorkItem.module.scss";
+
+interface WorkItemProps {
+  link: string;
+  title: string;
+  description: string;
+  className?: string;
+}
+
+const WorkItem = ({
+  link,
+  title,
+  description,
+  className,
+  ...other
+}: WorkItemProps) => (
+  <div className={`${styles.container} ${className}`} {...other}>
+    <LinkTitle className={styles.linkOverrides} title={title} link={link} />
+    <p>{description}</p>
+  </div>
+);
+
+export default WorkItem;
