7dfdf1f66725969b5340818a123f5ea56d5edd12
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-20 14:42:12 -0400

    feat: add placeholder content for extracurriculars

---
+++ src/content/extracurriculars.tsx
@@ -0,0 +1,10 @@
+const extracurriculars = Array(5).fill({
+  title: "Lorem Ipsum Dolor",
+  link: "https://example.com/path/extracurricular",
+  technologies: ["Technology A", "Technology B", "Technology C"],
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a nisi a velit malesuada pulvinar quis eu mauris. In sed ultrices ex, suscipit pharetra mi. Nunc in nisl odio. Proin auctor dolor id elementum aliquet.",
+  thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+});
+
+export default extracurriculars;
