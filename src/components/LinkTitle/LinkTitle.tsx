f119dac4a1479d29b0bcab96d508e8a7fa116dbd
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-19 21:39:59 -0400

    fix: make sure links open in a new tab

--- src/components/LinkTitle/LinkTitle.tsx
+++ src/components/LinkTitle/LinkTitle.tsx
@@ -8,7 +8,13 @@ interface LinkTitleProps {
 }
 
 const LinkTitle = ({ title, link, className, ...other }: LinkTitleProps) => (
-  <a href={link} className={`${styles.linkContainer} ${className}`} {...other}>
+  <a
+    href={link}
+    className={`${styles.linkContainer} ${className}`}
+    rel="noreferrer"
+    target="_blank"
+    {...other}
+  >
     <p>{formatLink(link)}</p>
     <h3>{title}</h3>
   </a>
