6312d0a75eb71b9c4d05f4665c2cb35f9614c58d
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-20 10:47:42 -0400

    feat: add skills page

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,3 +1,37 @@
-const Skills = () => <h1>Skills</h1>;
+import FadeIn from "react-fade-in";
+
+import Chip from "components/Chip";
+import ResultCount from "components/ResultCount";
+import skills from "content/skills";
+
+import styles from "./Skills.module.scss";
+
+const Skills = () => {
+  let count = 0;
+  skills.forEach(({ items }) => (count += items.length));
+
+  return (
+    <main>
+      <ResultCount count={count} />
+      <FadeIn className={styles.container}>
+        {skills.map(({ title, items }) => (
+          <>
+            <h3 className={styles.sectionTitle}>{title}</h3>
+            <FadeIn className={styles.chipContainer}>
+              {items.map((skill) => (
+                <Chip
+                  key={skill}
+                  text={skill}
+                  className={styles.chipOverrides}
+                />
+              ))}
+            </FadeIn>
+            <div className={styles.sectionSpacer} />
+          </>
+        ))}
+      </FadeIn>
+    </main>
+  );
+};
 
 export default Skills;
