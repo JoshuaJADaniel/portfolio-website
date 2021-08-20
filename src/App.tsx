fc7edfeb65a98347ffa14aac51a2fdfe23344164
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-20 13:19:03 -0400

    feat: add footer to main layout

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,34 @@ import Images from "pages/Images";
 import Skills from "pages/Skills";
 import Extracurriculars from "pages/Extracurriculars";
 
-import Header from "components/Header";
 import Menu from "components/Menu";
+import Header from "components/Header";
+import Footer from "components/Footer";
 
 const App = () => (
   <Router>
-    <Header />
-    <Menu />
-    <Switch>
-      <Route exact path="/">
-        <All />
-      </Route>
-      <Route exact path="/images">
-        <Images />
-      </Route>
-      <Route exact path="/skills">
-        <Skills />
-      </Route>
-      <Route exact path="/extracurriculars">
-        <Extracurriculars />
-      </Route>
-      <Route path="*">
-        <Redirect to="/" />
-      </Route>
-    </Switch>
+    <main>
+      <Header />
+      <Menu />
+      <Switch>
+        <Route exact path="/">
+          <All />
+        </Route>
+        <Route exact path="/images">
+          <Images />
+        </Route>
+        <Route exact path="/skills">
+          <Skills />
+        </Route>
+        <Route exact path="/extracurriculars">
+          <Extracurriculars />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </main>
+    <Footer />
   </Router>
 );
 
