e985a090c3385996b5534cabeb0178dbf63958f2
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-20 13:19:19 -0400

    feat: add proper source code link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,11 +2,14 @@ import styles from "./Footer.module.scss";
 
 const Footer = () => (
   <footer className={styles.container}>
-    <p>
-      Copyright &copy; {new Date().getFullYear()} Joshua Daniel. All rights
-      reserved.
-    </p>
-    <a href="#">View Source Code</a>
+    <p>Copyright &copy; 2021 Joshua Daniel. All rights reserved.</p>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://github.com/joshuajadaniel/portfolio-website"
+    >
+      View Source Code
+    </a>
   </footer>
 );
 
