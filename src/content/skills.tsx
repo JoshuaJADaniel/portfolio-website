ac2eb56627fc3d2daec19b6c213f8e19ca06e8c0
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-20 10:47:01 -0400

    feat: add placeholder skills content

---
+++ src/content/skills.tsx
@@ -0,0 +1,28 @@
+const skills = [
+  {
+    title: "Programming Languages",
+    items: [
+      "Java",
+      "JavaScript",
+      "PHP",
+      "Python",
+      "ReactJS",
+      "jQuery",
+      "C",
+      "C#",
+      "HTML5",
+      "CSS3",
+      "Sass/SCSS",
+    ],
+  },
+  {
+    title: "Database",
+    items: ["A", "B", "C"],
+  },
+  {
+    title: "Tools and Technologies",
+    items: ["A", "B", "C"],
+  },
+];
+
+export default skills;
