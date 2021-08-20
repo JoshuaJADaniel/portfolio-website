d3e945c3532c739472a597a26199be832b4722ef
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-19 21:36:04 -0400

    feat: add primary landing page

--- src/pages/All/All.tsx
+++ src/pages/All/All.tsx
@@ -1,3 +1,34 @@
-const All = () => <h1>All</h1>;
+import FadeIn from "react-fade-in";
+
+import ResultCount from "components/ResultCount";
+import KnowledgePanel from "components/KnowledgePanel";
+import ProjectItem from "components/ProjectItem";
+import WorkItem from "components/WorkItem";
+
+import experiences from "content/experiences";
+import projects from "content/projects";
+import profile from "content/profile";
+
+import styles from "./All.module.scss";
+
+const All = () => (
+  <main>
+    <ResultCount count={experiences.length + projects.length} />
+    <FadeIn className={styles.sidebar}>
+      <KnowledgePanel {...profile} />
+    </FadeIn>
+    <FadeIn className={styles.content}>
+      <h3 className={styles.sectionTitle}>Experience</h3>
+      {experiences.map((details) => (
+        <WorkItem className={styles.itemOverrides} {...details} />
+      ))}
+      <div className={styles.sectionSpacer} />
+      <h3 className={styles.sectionTitle}>Projects</h3>
+      {projects.map((details) => (
+        <ProjectItem className={styles.itemOverrides} {...details} />
+      ))}
+    </FadeIn>
+  </main>
+);
 
 export default All;
