9b78dc6282749f16189118dc06d6f310d744ba90
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-20 14:42:30 -0400

    feat: complete extracurriculars page

--- src/pages/Extracurriculars/Extracurriculars.tsx
+++ src/pages/Extracurriculars/Extracurriculars.tsx
@@ -1,8 +1,17 @@
 import Layout from "components/Layout";
+import ProjectItem from "components/ProjectItem";
+import extracurriculars from "content/extracurriculars";
+import styles from "./Extracurriculars.module.scss";
 
 const Extracurriculars = () => (
-  <Layout count={0}>
-    <h1>Extracurriculars</h1>
+  <Layout count={extracurriculars.length}>
+    {extracurriculars.map((details) => (
+      <ProjectItem
+        key={details.title}
+        className={styles.itemOverrides}
+        {...details}
+      />
+    ))}
   </Layout>
 );
 
