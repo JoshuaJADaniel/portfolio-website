c52a3cb7e2a39ae0221332bafe3934ed2852d941
Joshua Daniel <joshuajadaniel@gmail.com>
2021-09-03 11:48:42 -0400

    feat: update extracurriculars with proper info

--- src/content/extracurriculars.tsx
+++ src/content/extracurriculars.tsx
@@ -1,10 +1,53 @@
-const extracurriculars = Array(5).fill({
-  title: "Lorem Ipsum Dolor",
-  link: "https://example.com/path/extracurricular",
-  technologies: ["Technology A", "Technology B", "Technology C"],
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a nisi a velit malesuada pulvinar quis eu mauris. In sed ultrices ex, suscipit pharetra mi. Nunc in nisl odio. Proin auctor dolor id elementum aliquet.",
-  thumbnail: "https://dummyimage.com/600x400/000be0/fff",
-});
+const extracurriculars = [
+  {
+    title: "Microsoft Certified: Azure Cloud AZ-900",
+    link: "https://www.credly.com/badges/6bdb5c6f-e5f7-4a46-a0a2-f13912cddd1f",
+    thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+    technologies: ["Cloud", "Pipelines", "DevOps", "Containers"],
+    description: (
+      <>
+        Certification of knowledge in Azure Cloud core concepts, services,
+        workloads, administration, security, privacy, etc.
+      </>
+    ),
+  },
+  {
+    title: "Carnegie Mellon: picoCTF Hackathon",
+    link: "https://picoctf.org/about",
+    thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+    technologies: ["C", "C++", "SQL", "Bash", "Python"],
+    description: (
+      <>
+        Placed 10th nationally and 26th internationally out of 110,000+
+        participants at a cybersecurity hackathon centered on engineering,
+        penetration testing, and decryption.
+      </>
+    ),
+  },
+  {
+    title: "UOIT: Engineering Robotics Competition",
+    link: "https://engineering.ontariotechu.ca",
+    thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+    technologies: ["EV3", "RBT"],
+    description: (
+      <>
+        Competed at UOIT’s robotics competition and received 4th place
+        distinction against all schools in Ontario.
+      </>
+    ),
+  },
+  {
+    title: "HLSS: Lead Senior Mathematics Tutor",
+    link: "https://www.peelschools.org/schools/heartlake",
+    thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+    technologies: ["Calculus", "Linear Algebra", "Advanced Functions"],
+    description: (
+      <>
+        Directed daily math tutoring sessions for Calculus / Linear Algebra and
+        managed a team of 6-12 junior math tutors.
+      </>
+    ),
+  },
+];
 
 export default extracurriculars;
