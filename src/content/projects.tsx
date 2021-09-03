ed20711390b02a1e4e8516b2a84101bbd2f37688
Joshua Daniel <joshuajadaniel@gmail.com>
2021-09-03 11:48:54 -0400

    feat: update projects with proper info

--- src/content/projects.tsx
+++ src/content/projects.tsx
@@ -1,10 +1,64 @@
-const projects = Array(5).fill({
-  title: "Lorem Ipsum Dolor",
-  link: "https://example.com/path/project",
-  technologies: ["Technology A", "Technology B", "Technology C"],
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a nisi a velit malesuada pulvinar quis eu mauris. In sed ultrices ex, suscipit pharetra mi. Nunc in nisl odio. Proin auctor dolor id elementum aliquet.",
-  thumbnail: "https://dummyimage.com/600x400/000be0/fff",
-});
+const projects = [
+  {
+    title: "Medical Clinic App",
+    link: "https://github.com/joshuajadaniel/medical-clinic-app",
+    thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+    technologies: ["Java", "NoSQL", "Firebase", "Mockito", "JUnit"],
+    description: (
+      <>
+        A mobile Android app that allows patients and doctors to book
+        appointments, view availabilities, setup schedules, etc.
+      </>
+    ),
+  },
+  {
+    title: "Image Repository",
+    link: "https://github.com/joshuajadaniel/image-repository",
+    thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+    technologies: ["PHP", "MySQL", "React.js", "JWT"],
+    description: (
+      <>
+        A web-based repository where users can publicly source stock images and
+        secure private uploads in cloud storage.
+      </>
+    ),
+  },
+  {
+    title: "Cursorlux",
+    link: "https://github.com/joshuajadaniel/cursorlux",
+    thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+    technologies: ["JavaScript", "React.js", "Redux", "Jest", "RTL"],
+    description: (
+      <>
+        A Chrome extension to highlight users’ cursor and clicks seen by over
+        12,800+ unique users.
+      </>
+    ),
+  },
+  {
+    title: "Google Search",
+    link: "https://github.com/joshuajadaniel/portfolio-website",
+    thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+    technologies: ["TypeScript", "Docker", "Jenkins", "React.js"],
+    description: (
+      <>
+        A portfolio website in the style of a Google Search result built with a
+        Jenkins CI/CD pipeline in a Docker container.
+      </>
+    ),
+  },
+  {
+    title: "Videoverse",
+    link: "https://github.com/joshuajadaniel/videoverse",
+    thumbnail: "https://dummyimage.com/600x400/000be0/fff",
+    technologies: ["React.js", "Sass/SCSS", "Axios"],
+    description: (
+      <>
+        A realtime web application where users can browse 800,000+ trending
+        movies, shows, actors, etc.
+      </>
+    ),
+  },
+];
 
 export default projects;
