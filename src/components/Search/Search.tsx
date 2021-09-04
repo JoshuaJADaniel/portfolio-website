1646fa50fabafd07b25dfab73a470de0a5cd4eb9
Joshua Daniel <joshuajadaniel@gmail.com>
2021-09-03 23:13:20 -0400

    fix: add title to search for accessibility

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,7 +4,12 @@ import styles from "./Search.module.scss";
 
 const Search = () => (
   <div className={styles.container}>
-    <input readOnly value="Joshua Daniel" className={styles.inputField} />
+    <input
+      readOnly
+      title="search"
+      value="Joshua Daniel"
+      className={styles.inputField}
+    />
     <img alt="Microphone Icon" src={imgMic} className={styles.controlButton} />
     <img alt="Search Icon" src={imgSearch} className={styles.controlButton} />
   </div>
